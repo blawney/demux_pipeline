7b871ca3a91e3b88173fcbcc993b6a3176926242
Brian Lawney <blawney@joshua.dfci.harvard.edu>
2015-02-24 16:14:43 -0500

    Completed up to kicking off fastQC process.  Still need tests.

--- process_sequencing_run.py
+++ process_sequencing_run.py
@@ -5,6 +5,7 @@ import os
 import re
 import subprocess
 import shutil
+import glob
 from datetime import datetime as date
 from ConfigParser import SafeConfigParser
 
@@ -70,12 +71,19 @@ def line_is_valid(line, sample_dir_prefix):
 	"""
 	elements = line.split(',')
 
-	tests = []
-	tests.append(elements[0].startswith(sample_dir_prefix)) # check that the Sample_ID field has the proper prefix
-	tests.append(elements[0].strip(sample_dir_prefix) == elements[1]) # check that the sample names match between the first and second fields
-	tests.append(len(elements[5]) == 7) # check that 7bp index was provided
-	tests.append(len(elements[6])>0) # check that there is an entry in the Sample_Project field.  Important for splitting FASTQs between projects
-	return all(tests)
+	# ensure the sample name only has letters, numbers, and underscores with a greedy regex.  No other characters allowed.
+	match = re.match('[a-z0-9_]+', elements[1], re.IGNORECASE)
+
+	# if the greedy regex did not match the sample name, then something is wrong.  Only proceed if the name is valid 
+	if match and match.group() == elements[1]:
+		tests = []
+		tests.append(elements[0].startswith(sample_dir_prefix)) # check that the Sample_ID field has the proper prefix
+		tests.append(elements[0].strip(sample_dir_prefix) == elements[1]) # check that the sample names match between the first and second fields
+		tests.append(len(elements[5]) == 7) # check that 7bp index was provided
+		tests.append(len(elements[6])>0) # check that there is an entry in the Sample_Project field.  Important for splitting FASTQs between projects
+		return all(tests)
+	else:
+		return False
 
 
 
@@ -125,6 +133,58 @@ def run_demux(bcl2fastq2_path, run_dir_path, output_dir_path):
 
 
 
+def concatenate_fastq_files(output_directory_path, project_id_list, sample_dir_prefix):
+	"""
+	This method scans the output and concatenates the fastq files for each sample and read number.
+	That is, the NextSeq output (in general) has a fastq file for each lane and sample and we need to 
+	concatenate the lane files into a single fastq file for that sample.  For paired-end protocol, need
+	to do this for both the R1 and R2 reads.
+	"""
+
+	for project in project_id_list:
+		project_dir = os.path.join(output_directory_path, project)
+		
+		# get the sample-specific subdirectories and append the path to the project directory in front (for full path name):
+		sample_dirs = [os.path.join(project_dir, d) for d in os.listdir(project_dir) if d.startswith(sample_dir_prefix)]
+
+		# double check that they are actually directories:
+		sample_dirs = filter( lambda x: os.path.isdir(x), sample_dirs)
+
+		for sample_dir in sample_dirs:
+
+			# since bcl2fastq2 renames the fastq files with a different scheme, extract the sample name we want via parsing the directory name
+			sample_name = os.path.basename(sample_dir).strip(sample_dir_prefix)
+
+			# get all the fastq files as lists.  Note the sort so they are concatenated in the same order for paired-end protocol
+			read_1_fastq_files = sorted(glob.glob(os.path.join(sample_dir, '*R1_001.fastq.gz')))			
+			read_2_fastq_files = sorted(glob.glob(os.path.join(sample_dir, '*R2_001.fastq.gz'))) # will be empty list [] if single-end protocol
+
+			# need at least the read 1 files to continue
+`			if len(read_1_fastq_files) == 0:
+				print 'Did not find any fastq files in %s directory.' % sample_dir
+				sys.exit(1)			
+
+			# make file names for the merged files:
+			merged_read_1_fastq = sample_name + '_R1_001.fastq.gz'
+			merged_read_2_fastq = sample_name + '_R2_001.fastq.gz'
+
+			# an inline method to compose the concatenation command:
+			write_call = lambda infiles,outfile: 'cat '+ ' '.join(infiles) + ' >' + outfile
+
+			call_list = []
+			call_list.append(write_call(read_1_fastq_files, merged_read_1_fastq))
+			if len(read_2_fastq) > 0:
+				call_list.append(write_call(read_1_fastq_files, merged_read_1_fastq))
+			
+			for call in call_list:
+				try:
+					subprocess.check_call( call, shell = True )			
+				except subprocess.CalledProcessError:
+					print 'The concatentation of the lane-specific fastq files failed somehow'
+					sys.exit(1)
+					
+
+
 def relocate_fastq_files(src_dir, projects_dir, project_id_list):
 	"""
 	This function uses the project_id_list to move the project directories to another location
@@ -156,6 +216,21 @@ def relocate_fastq_files(src_dir, projects_dir, project_id_list):
 		else:
 			print 'Target directory %s does not exist for some reason.  Maybe permissions?' % target_dir		
 
+
+
+
+def correct_permissions(bcl2fastq2_outdir):
+	"""
+	Gives write privileges to the biocomp group (if not already there)
+	"""
+	for root, dirs, files in os.walk(bcl2fastq2_outdir):
+		for d in dirs:
+			os.chmod(os.path.join(root, d), 0774)
+		for f in files:
+			os.chmod(os.path.join(root, f), 0774)
+
+
+
 def process():
 	config_params_dict = parse_config_file()
 
@@ -164,12 +239,15 @@ def process():
 	sample_dir_prefix = config_params_dict('sample_dir_prefix')
 	final_destination_path = config_params_dict.get('destination_path')
 
-
 	run_directory_path = parse_commandline_args()
 	output_directory_path = create_output_directory(run_directory_path, bcl2fastq2_output_dir)
 	project_id_list = check_samplesheet(run_directory_path, sample_dir_prefix)
 	run_demux(bcl2fastq2_path, run_directory_path, output_directory_path)
+	correct_permissions(output_directory_path)
+	concatenate_fastq_files(output_directory_path, project_id_list, sample_dir_prefix)
 	relocate_fastq_files(output_directory_path, final_destination_path, project_id_list)
+
+
 
 if __name__ == '__main__':
 	process()
